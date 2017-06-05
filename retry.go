9e24e21071f2c9dffe2325e8be78c5ff6709da3b
Rafael Jesus <rafaelljesus86@gmail.com>
2017-06-05 18:07:37 +0200

    Tiny refactoring

--- retry.go
+++ retry.go
@@ -10,13 +10,13 @@ type function func() error
 // Do runs the passed function until the number of retries is reached.
 func Do(fn function, retries int, sleep time.Duration) error {
 	if err := fn(); err != nil {
-		if retries--; retries > 0 {
-			jitter := time.Duration(rand.Int63n(int64(sleep)))
-			sleep = sleep + jitter/2
-			time.Sleep(sleep)
-			return Do(fn, retries, 2*sleep)
+		retries--
+		if retries == 0 {
+			return err
 		}
-		return err
+		sleep += (time.Duration(rand.Int63n(int64(sleep)))) / 2
+		time.Sleep(sleep)
+		return Do(fn, retries, 2*sleep)
 	}
 
 	return nil
